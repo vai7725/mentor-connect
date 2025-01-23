2e786bf21422edcbbf089b0daf84c4872cec532d
Vaibhav Prajapat <vaibhav.prajapat.7725@gmail.com>
2025-01-23 21:53:06 +0530

    Auth done

--- src/app/auth/callback/page.tsx
+++ src/app/auth/callback/page.tsx
@@ -1,12 +1,13 @@
-import { onAuthenticateUser } from "@/actions/user";
-import { redirect } from "next/navigation";
+import { onAuthenticateUser } from '@/actions/user'
+import { redirect } from 'next/navigation'
 
 const AuthCallbackPage = async () => {
-  const auth = await onAuthenticateUser();
-  if (auth.status === 200 || auth.status === 201) return redirect(`/dashboard`);
+  const auth = await onAuthenticateUser()
+  console.log(auth)
+  if (auth.status === 200 || auth.status === 201) return redirect(`/dashboard`)
 
   if (auth.status === 403 || auth.status === 400 || auth.status === 500)
-    return redirect("/auth/sign-in");
-};
+    return redirect('/auth/sign-in')
+}
 
-export default AuthCallbackPage;
+export default AuthCallbackPage
